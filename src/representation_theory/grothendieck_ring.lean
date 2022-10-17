57369a3aa5958b7ad21480003cf721b3fc7099cd
Daan van Gent <daanvangent@gmail.com>
2022-10-17 12:40:08 +0200

    added a bunch of sorries

--- src/representation_theory/grothendieck_ring.lean
+++ src/representation_theory/grothendieck_ring.lean
@@ -9,67 +9,112 @@ import algebra.direct_sum.basic
 open_locale direct_sum
 open ideal
 
-theorem maximal_quot_is_simple {R : Type*} [ring R] {I : ideal R} (h : is_maximal I) :
-  is_simple_module R (R ⧸ I) := is_simple_module_iff_is_coatom.mpr h.out
+classical
 
-structure max_spec (R : Type*) [ring R] := (ideal : ideal R) (is_maximal : is_maximal ideal)
+-- Some properties of morphisms
+section morphisms
 
-def submodule_inclusion {R M : Type*} [ring R] [add_comm_monoid M] [module R M] (A : submodule R M) :
-  A →+ M := add_submonoid_class.subtype A
+variables {R M N : Type*} [ring R] [add_comm_group M] [module R M] [add_comm_group N]
+  [module R N] (f : M →ₗ[R] N)
 
-def image_restriction {R M N : Type*} [ring R] [add_comm_monoid M] [add_comm_monoid N] [module R M]
-  [module R N] (I : submodule R N) (f : M →+ N) (h : set.image f ⊤ ≤ I ) : M →+ I :=
+abbreviation im : submodule R N := submodule.map f ⊤
+abbreviation ker : submodule R M := submodule.comap f ⊥
+abbreviation coker := N ⧸ im f
+
+end morphisms
+
+-- Some properties of simple modules
+section simple
+
+instance nontrivial_of_simple {R M : Type*} [ring R] [add_comm_group M] [module R M]
+  (h : is_simple_module R M) : nontrivial M :=
 begin
-  refine add_monoid_hom.cod_restrict f I _,
-  intro x,
+  rcases h.to_nontrivial with ⟨a, b, hab⟩,
+  have hab' : a.carrier ≠ b.carrier, {
+    intro h,
+    apply hab,
+    cases a,
+    cases b,
+    simp only,
+    exact h,
+  },
+  have h' : ¬ ( a.carrier ⊆ b.carrier ∧ b.carrier ⊆ a.carrier ), {
+    intro h,
+    apply hab',
+    sorry,
+  },
+  have h'' : (¬ a.carrier ⊆ b.carrier) ∨ (¬ b.carrier ⊆ a.carrier), {
+    sorry,
+  },
+  wlog h''' : (¬ a.carrier ⊆ b.carrier) using a b,
+  exact h'',
   sorry,
 end
 
-def subsubmodule_inclusion {R M :Type*} [ring R] [add_comm_monoid M] [module R M] (A B : submodule R M)
-  (h : A ≤ B) : A →+ B :=
+lemma nonzero_of_nontrivial {M : Type*} [add_comm_group M] (t : nontrivial M) : ∃ x : M, x ≠ 0 :=
 begin
-  refine add_monoid_hom.restrict _ A,
-  apply image_restriction,
-  swap,
-  split,
-  rotate 2,
-  exact id,
-  suggest,
+  have t' := t,
+  rcases t' with ⟨a, b, hab⟩,
+  use a-b,
+  intro h,
+  apply hab,
+  sorry,
 end
 
-def submodule_coe {R M : Type*} [ring R] [add_comm_monoid M] [module R M]
-  (A B : submodule R M) (h : A ≤ B) : submodule R B := {
-  carrier := set.preimage (@coe B M _) A,
-  add_mem' := λ a b, submodule.add_mem A,
-  zero_mem' := _,
-  smul_mem' := _ }
+end simple
 
-instance jordan_holder_module {R M : Type*} [ring R] [add_comm_monoid M] [module R M] :
+-- Jordan Holder
+section JordanHolder
+
+open submodule
+
+instance jordan_holder_module {R M : Type*} [ring R] [add_comm_group M] [module R M] :
   jordan_holder_lattice (submodule R M) := {
-  is_maximal := λ A B, (A ≤ B) ∧ (A ≤ B → A ⧸ B),
-  lt_of_is_maximal := _,
-  sup_eq_of_is_maximal := _,
-  is_maximal_inf_left_of_is_maximal_sup := _,
-  iso := _,
-  iso_symm := _,
-  iso_trans := _,
-  second_iso := _
+  is_maximal := λ A B, (A ≤ B) ∧
+    (∀ h : A ≤ B, is_simple_module R (coker $ of_le h)),
+  lt_of_is_maximal := λ A B h, begin
+    apply lt_of_le_of_ne h.1,
+    intro hAB,
+    cases nonzero_of_nontrivial (nontrivial_of_simple $ h.2 h.1) with x hx,
+    apply hx,
+    -- let z := quotient.exists_rep x,
+    sorry,
+  end,
+  sup_eq_of_is_maximal := sorry,
+  is_maximal_inf_left_of_is_maximal_sup := sorry,
+  iso := λ X Y, ∀ hX : X.1 ≤ X.2, ∀ hY : Y.1 ≤ Y.2,
+    nonempty $ (coker (of_le hX)) ≃ₗ[R] coker (of_le hY),
+  iso_symm := begin
+    sorry,
+  end,
+  iso_trans := sorry,
+  second_iso := sorry
 }
 
-lemma finite_composition_series_of_artinian {R : Type*} [ring R] (M : Type*) [add_comm_monoid M]
+end JordanHolder
+
+-- Composition serier
+section composition_series
+
+lemma finite_composition_series_of_artinian {R : Type*} [ring R] (M : Type*) [add_comm_group M]
   [module R M] [is_artinian R M] (N : submodule R M) :
   (λ s : composition_series (submodule R M), s.top).surjective :=
 begin
   sorry,
 end
 
+end composition_series
 
+-- Grothendieck ring
+section grothendieck
+
+structure max_spec (R : Type*) [ring R] := (ideal : ideal R) (is_maximal : is_maximal ideal)
 def grothendieck_ring (R : Type*) [ring R] := (max_spec R) →₀ ℤ
 
-section
-variables (R : Type*) [ring R] (M : Type*) [add_comm_monoid M] [module R M] [is_artinian R M]
+namespace grothendieck
+variables (R : Type*) [ring R] (M : Type*) [add_comm_group M] [module R M] [is_artinian R M]
 
-def grothendieck_ring_mk'' (S : composition_series (submodule R M)) : grothendieck_ring R :=
+def mk'' (S : composition_series (submodule R M)) : grothendieck_ring R :=
 begin
   -- take quotients
   -- convert to maximal ideals
@@ -77,14 +122,14 @@ begin
   sorry
 end
 
-def grothendieck_ring_mk' (N : submodule R M) :
-  grothendieck_ring R :=
+def mk' (N : submodule R M) : grothendieck_ring R :=
 begin
   -- pick a composition series for N
   -- prove that well defined up to equivalence
   sorry
 end
 
-def grothendieck_ring_mk : grothendieck_ring R := grothendieck_ring_mk' R M ⊤
+def mk : grothendieck_ring R := mk' R M ⊤
 
-end
+end grothendieck
+end grothendieck
