76d9764729bde2805d6a4f2fd7140919d6562cd6
jessetvogel <jesse.t.vogel@gmail.com>
2022-10-19 13:42:30 +0200

    Moved file p_group_solvale.lean to sorry branch

---
+++ src/representation_theory/p_group_solvable.lean
@@ -0,0 +1,8 @@
+import group_theory.solvable group_theory.p_group
+
+variables (p : ℕ) (G : Type*) [group G]
+
+theorem p_group_solvable (hG : is_p_group p G) : is_solvable G :=
+begin
+  sorry,
+end
