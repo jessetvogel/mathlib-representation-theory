7ee36a80fffad29c60d38114e155d6bf53d6e932
jessetvogel <jesse.t.vogel@gmail.com>
2022-10-26 17:13:40 +0200

    Continued with to p_group_solvable, largely solved.

--- src/representation_theory/p_group_solvable.lean
+++ src/representation_theory/p_group_solvable.lean
@@ -2,6 +2,18 @@ import group_theory.solvable
 import group_theory.p_group
 import tactic.basic
 
+theorem solvable_of_solvable_quotient_center
+  {G : Type*} [group G] (h : is_solvable (G ⧸ subgroup.center G)) : is_solvable G := by {
+  sorry,
+}
+
+noncomputable instance {G : Type*} [group G] [finite G] (N : subgroup G) : fintype (G ⧸ N) := fintype.of_finite _
+
+lemma card_quotient_lt
+  {G : Type*} [group G] [fintype G] (N : subgroup G) [nontrivial N] : fintype.card (G ⧸ N) < fintype.card G := by {
+  sorry,
+}
+
 variables {G : Type*} [group G] [fintype G]
 
 lemma top_eq_bot_of_trivial (h : fintype.card G = 1) : ⊥ = (⊤ : subgroup G) := by {
@@ -15,37 +27,65 @@ instance solvable_of_trivial (h : fintype.card G = 1) : is_solvable G :=
 
 variables (p : ℕ) [fact (nat.prime p)]
 
-#check (infer_instance : nonempty G)
--- #check (infer_instance : nontrivial G)
+lemma nat.le_of_le_succ_and_ne {n m : ℕ} (h₁ : n ≤ m + 1) (h₂ : n ≠ m + 1) : n ≤ m := by {
+  sorry,
+}
 
 theorem p_group_solvable (hG : is_p_group p G) : is_solvable G :=
 begin
-  -- Strong induction on the cardinality of G
-  let q : ∀ n, fintype.card G ≤ n → is_solvable G := by {
-    -- Base case: |G| = 0
-    intros n hn,
-    induction n with m hm,
+  -- Apply induction on n to the statement that all p-groups K with |K| ≤ n are solvable.
+  let q : ∀
+    (n : ℕ)
+    (K : Type*)
+    [K_group : group K]
+    [K_fin : fintype K]
+    [@is_p_group p K K_group]
+    (hK : @fintype.card K K_fin ≤ n), @is_solvable K K_group := by {
+    intro n;
+    induction n with n hn,
+    -- Base case: n = 0
+    introsI K K_group K_fin K_p_group hK,
     exfalso,
-    have q' : fintype.card G ≠ 0 := fintype.card_ne_zero,
-    exact q' (nat.eq_zero_of_le_zero hn),
-    -- Induction step:
-    by_cases fintype.card G = m.succ, {
-      clear hn,
-      cases m,
-      -- Case |G| = 1,
-      apply solvable_of_trivial h,
-      -- Case |G| > 1,
-      haveI G_nontrivial : nontrivial G := sorry,
-      have Z_nontrivial := is_p_group.center_nontrivial hG,
-
-      sorry
+    have q' : fintype.card K ≠ 0 := fintype.card_ne_zero,
+    exact q' (nat.eq_zero_of_le_zero hK),
+    -- Induction step: n > 0
+    introsI K K_group K_fin K_p_group hK,
+    -- Distinguish cases |K| = n + 1 and |K| ≠ n + 1
+    by_cases card_K : (fintype.card K = n.succ), {
+      -- Case |K| = n + 1.
+      -- Distinguish cases n = 0 and n ≠ 0
+      by_cases n_zero : (n = 0), {
+        -- Case n = 0, then |K| = 1, so K is trivial, and hence solvable
+        rw [n_zero] at card_K,
+        exact solvable_of_trivial card_K,
+      }, {
+        -- Case n ≠ 0, then |K| > 1, so K is non-trivial.
+        haveI K_nontrivial : nontrivial K := by {
+          sorry,
+        },
+        -- Then the center Z(K) is non-trivial
+        haveI Z_nontrivial := is_p_group.center_nontrivial K_p_group,
+        -- Construct the quotient K / Z(K)
+        let H := K ⧸ (subgroup.center K),
+        -- Then H is finite, and of cardinality smaller than G
+        have H_card_le_K_card : fintype.card H < fintype.card K := card_quotient_lt (subgroup.center K),
+        -- Hence, |K / Z(K)| ≤ n
+        have H_card_le_m_succ : fintype.card H ≤ n := by {
+          apply nat.le_of_lt_succ,
+          rw [← card_K],
+          exact H_card_le_K_card,
+        },
+        -- Also, any quotient of a p-group is a p-group
+        haveI H_p_group : is_p_group p H := is_p_group.to_quotient K_p_group _,
+        -- Thus, H is solvable
+        have H_solvable := @hn H _ _ H_p_group H_card_le_m_succ, -- TODO: fix this
+        exact solvable_of_solvable_quotient_center H_solvable,
+      }
     }, {
-      apply hm,
-      cases nat.of_le_succ hn with h₁ h₂,
-      exact h₁,
-      exfalso,
-      exact h h₂,
-    },
+      -- Case |K| ≠ n + 1. Then |K| ≤ n, so we can use the induction hypothesis.
+      apply @hn K K_group K_fin K_p_group,
+      apply nat.le_of_le_succ_and_ne hK card_K,
+    }
   },
-  exact q (fintype.card G) le_rfl,
+  exact @q (fintype.card G) G _ _ hG le_rfl,
 end
